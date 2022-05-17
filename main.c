cde342b6bed4152907e2cc120826f57d8c66017a
Jerzy Palusiński <105461178+JerzyPal@users.noreply.github.com>
2022-05-17 09:40:15 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+
 #define PROGMEM 1000
 #define ROW 10
-unsigned char mem[PROGMEM];
+#define LOOPMAX 256
+#define ENDBRACKET 1
+#define STARTBRACKET 0
+
+
+
+unsigned char mem[PROGMEM]={0};
 long int pointer = 0;
-void interpret(char letter)
+int loop[LOOPMAX][3] = {0};
+int loopcounter = -1;
+void interpret(char letter, int *exepoint)
 {
 
 
@@ -25,12 +36,20 @@ switch(letter)
         mem[pointer]--;
         break;
     case'.':
+        putchar(mem[pointer]);
         break;
     case',':
+        mem[pointer] = getchar();
         break;
     case'[':
+        if(mem[pointer] !=0 )
+        {
+        loopcounter++;
+        loop[loopcounter] = *exepoint;
+        }
         break;
     case']':
+
         break;
 
     }
@@ -56,23 +75,63 @@ return;
 }
 
 
+
+void loopprocessing(int *progpoint, char bracket)
+{
+
+    if (bracket == STARTBRACKET)
+    {
+    loop[loopcounter][1]=1;
+
+    }
+    else //ENDBRACKET
+    {
+    loop[loopcounter][1]=1;
+
+    }
+
+}
+
+
+
+
 //branduck
 int main()
 {
-    for(int i=0;i<PROGMEM;i++)
-        {
-            mem[i]=0;
-        }
-//    printMem();
+
 
     char prog[]="++++++++>++++>>>>>>>>>>+++>++<--";
+
+
+    /*
+
+     [ 0 *1
+     [ 1 *1
+     ] 1 *2
+     [ 2 *1
+     ] 2 *2
+     ] 0 *2
+     [ 3 *1
+
+
+    */
+
     for(int i=0;i<sizeof(prog)/sizeof(prog[0]);i++){
-    interpret(prog[i]);
+    if(prog[i]=='[')
+    loopprocessing(&i,STARTBRACKET);
+    else if(prog[i]==']')
+    loopprocessing(&i,ENDBRACKET);
+    }
+
+    loopcounter = -1;
+
+    for(int i=0;i<sizeof(prog)/sizeof(prog[0]);i++){
+    interpret(prog[i], &i);
 
     }
     printMem();
 
-printf("%d",PROGMEM);
+//printf("%d",PROGMEM);
 
 
 
